a6579899affe12975814d4f06ebcbb8f40c02b72
behcet <behcet.ilhan@hotmail.com>
2021-04-17 21:08:53 +0300

    added env variable for omdb api key

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,8 +1,2 @@
-/*
-URL AND KEY CAN BE MOVED TO .env
-process.env.REACT_APP_API_URL
-process.env.REACT_APP_API_KEY
-*/
-
 export const URL = 'https://www.omdbapi.com/';
-export const KEY = 'f0d03a08';
+export const KEY = process.env.REACT_APP_API_KEY;
