f2b55f12cf9419a65cda02e4041ccc96f9bb373a
behcet <behcet.ilhan@hotmail.com>
2021-04-17 22:07:36 +0300

    Update app name and icons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { debugContextDevtool } from 'react-context-devtool';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import MovieResultsProvider from './store/context';
